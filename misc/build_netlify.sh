337172fa8d0f5a27fc7d4ed9792f4a210ae2174b
saolsen <steve.olsen@gmail.com>
2018-11-29 22:33:27 -0500

    nightly:

--- misc/build_netlify.sh
+++ misc/build_netlify.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 curl https://sh.rustup.rs -sSf | sh -s -- -y
+rustup install nightly
+rustup default nightly
 source $HOME/.cargo/env
 curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
 cd website
